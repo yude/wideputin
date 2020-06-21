6617af6ba42dd3b2bc968d88af906ea07a3c95e3
yude <yu@yude.moe>
2020-06-21 19:06:38 +0900

    Added play, pause, speed function

--- wideputin/Main.cpp
+++ wideputin/Main.cpp
@@ -6,68 +6,43 @@ void Main()
 	// 背景を水色にする
 	Scene::SetBackground(ColorF(0.8, 0.9, 1.0));
 
+	// Wide Putin の音声ファイルを用意
+	const Audio audio(U"assets/putin.m4a");
+	audio.play();
+	double speed = 1.0;
 	// 大きさ 60 のフォントを用意
-	const Font font(60);
-
-	// 猫のテクスチャを用意
-	const Texture cat(Emoji(U"🐈"));
-
-	// 猫の座標
-	Vec2 catPos(640, 450);
+	const Font font(60, Typeface::Default, FontStyle::Italic);
+	const Font fontPos(30);
+	// プーチンのテクスチャを用意
+	const Texture putin(U"assets/putin.png");
 
 	while (System::Update())
 	{
 		// テキストを画面の中心に描く
-		font(U"Hello, Siv3D!🐣").drawAt(Scene::Center(), Palette::Black);
+		font(U"Wide Putin").drawAt(Scene::Center(), Palette::Black);
+		// 座標決め用
+		fontPos(U"x: {} y: {}"_fmt(Cursor::Pos().x, Cursor::Pos().y)).drawAt(Vec2(640, 580));
 
-		// 大きさをアニメーションさせて猫を表示する
-		cat.resized(100 + Periodic::Sine0_1(1s) * 20).drawAt(catPos);
-
-		// マウスカーソルに追従する半透明の赤い円を描く
-		Circle(Cursor::Pos(), 40).draw(ColorF(1, 0, 0, 0.5));
-
-		// [A] キーが押されたら
-		if (KeyA.down())
+		// プーチンの画像をマウスカーソルに追従させて描画する
+		putin.scaled(0.5).draw(Cursor::Pos());
+		if (SimpleGUI::Button(U"Play", Vec2(20, 20)))
 		{
-			// Hello とデバッグ表示する
-			Print << U"Hello!";
+			// 再生
+			audio.play();
 		}
-
-		// ボタンが押されたら
-		if (SimpleGUI::Button(U"Move the cat", Vec2(600, 20)))
+		if (SimpleGUI::Button(U"Pause", Vec2(20, 60)))
 		{
-			// 猫の座標を画面内のランダムな位置に移動する
-			catPos = RandomVec2(Scene::Rect());
+			// 一時停止
+			audio.pause();
+		}
+		if (SimpleGUI::Slider(U"{:.2f}"_fmt(speed), speed, 0.1, 2.0, Vec2(256,330)))
+		{
+			// 再生スピードを設定 (0.1 - 2.0)
+			audio.setSpeed(speed);
+		}
+		if (SimpleGUI::Button(U"Reset", Vec2(464, 330)))
+		{
+			audio.setSpeed(1.0);
 		}
 	}
 }
-
-//
-// = アドバイス =
-// Debug ビルドではプログラムの最適化がオフになります。
-// 実行速度が遅いと感じた場合は Release ビルドを試しましょう。
-// アプリをリリースするときにも、Release ビルドにするのを忘れないように！
-//
-// 思ったように動作しない場合は「デバッグの開始」でプログラムを実行すると、
-// 出力ウィンドウに詳細なログが表示されるので、エラーの原因を見つけやすくなります。
-//
-// = お役立ちリンク =
-//
-// OpenSiv3D リファレンス
-// https://siv3d.github.io/ja-jp/
-//
-// チュートリアル
-// https://siv3d.github.io/ja-jp/tutorial/basic/
-//
-// よくある間違い
-// https://siv3d.github.io/ja-jp/articles/mistakes/
-//
-// サポートについて
-// https://siv3d.github.io/ja-jp/support/support/
-//
-// Siv3D ユーザコミュニティ Slack への参加
-// https://siv3d.github.io/ja-jp/community/community/
-//
-// 新機能の提案やバグの報告
-// https://github.com/Siv3D/OpenSiv3D/issues
-//
